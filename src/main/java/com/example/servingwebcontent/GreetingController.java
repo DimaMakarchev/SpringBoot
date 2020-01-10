2301fb193053e3539b5fa4ee15dab813f7ddd2ab
Dima Makarchev <d.makarchev2411@gmail.com>
2020-01-10 23:31:30 +0300

    Spring4Jpa

--- src/main/java/com/example/servingwebcontent/GreetingController.java
+++ src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -1,24 +1,48 @@
 package com.example.servingwebcontent;
 
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private UserRepository userRepository;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    @GetMapping
+    public String methodOne(Model model) {
+        Iterable<User> all = userRepository.findAll();
+        model.addAttribute("bro", all);
         return "greeting";
     }
 
-    @GetMapping
-    public String methodNew(@RequestParam(name = "bro",required = false,defaultValue = "Cool Bro") String name,Model model){
-        model.addAttribute("bro",name);
-        model.addAttribute("bro1","Cool");
-        return "bro";
+    @PostMapping
+    public String add(@RequestParam String name, @RequestParam String email, Model model) {
+        User user = new User(name, email);
+        userRepository.save(user);
+        Iterable<User> all = userRepository.findAll();
+        model.addAttribute("bro", all);
+        return "greeting";
     }
+
+    @PostMapping("/coolbro")
+    public String methodFilter(@RequestParam String filter, Model model) {
+        Iterable<User> byName;
+        if (filter != null && !filter.isEmpty()) {
+            byName = userRepository.findByName(filter);
+        } else {
+            byName = userRepository.findAll();
+        }
+
+        model.addAttribute("bro", byName);
+        return "greeting";
+    }
+
 
 }
---
+++ src/main/java/com/example/servingwebcontent/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.servingwebcontent;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface UserRepository extends CrudRepository<User,Integer> {
+
+     List<User>findByName(String name);
+}
